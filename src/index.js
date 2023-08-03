b7114cf5ffef4f62484845960e32622f3c91bac2
Ev-a <evvrod@gmail.com>
2023-08-04 01:25:39 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  for (let i = 0; i < str.length; i += 1) {
+    console.log(isFirst(str[i], bracketsConfig));
+    if (isFirst(str[i], bracketsConfig)) {
+      if (isPare(stack[stack.length - 1], str[i], bracketsConfig)) {
+        stack.pop();
+      }
+      else {
+        stack.push(str[i]);
+      }
+    }
+    else {
+      if (stack.length === 0 || !isPare(stack[stack.length - 1], str[i], bracketsConfig)) {
+        return false;
+      } else {
+        stack.pop();
+      }
+    }
+  }
+  return stack.length === 0;
+}
+
+let isFirst = (a, bracketsConfig) => {
+  for (let i = 0; i < bracketsConfig.length; i += 1) {
+    if (bracketsConfig[i][0] === a) {
+      return true;
+    }
+  }
+  return false;
+}
+
+let isPare = (a, b, bracketsConfig) => {
+  for (let i = 0; i < bracketsConfig.length; i += 1) {
+    if (bracketsConfig[i][0] === a && bracketsConfig[i][1] === b) {
+      return true;
+    }
+  }
+  return false;
 }
